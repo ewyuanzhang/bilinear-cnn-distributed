3252accd78dd1be4447c32a6d237980751dd0bf4
Yuan Zhang <ewyuanzhang@gmail.com>
2019-02-22 00:19:38 -0800

    rearrange files & fix bugs

---
+++ src/bcnn.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Bilinear CNN model definition.
+
+This file is modified from:
+    https://github.com/HaoMood/blinear-cnn.
+"""
+
+
+import os
+
+import torch
+import torchvision
+
+torch.manual_seed(0)
+torch.cuda.manual_seed_all(0)
+
+
+__all__ = ['BCNN']
+__author__ = 'Yuan Zhang'
+__copyright__ = '2018 LAMDA'
+__date__ = '2019-02-22'
+__email__ = 'ewyuanzhang@gmail.com'
+__license__ = 'CC BY-SA 3.0'
+__status__ = 'Development'
+__updated__ = '2019-02-22'
+__version__ = '1.0'
+
+
+class BCNN(torch.nn.Module):
+    """B-CNN for CUB200.
+
+    The B-CNN model is illustrated as follows.
+    conv1^2 (64) -> pool1 -> conv2^2 (128) -> pool2 -> conv3^3 (256) -> pool3
+    -> conv4^3 (512) -> pool4 -> conv5^3 (512) -> bilinear pooling
+    -> sqrt-normalize -> L2-normalize -> fc (200).
+    The network accepts a 3*448*448 input, and the pool5 activation has shape
+    512*28*28 since we down-sample 5 times.
+
+    Attributes:
+        features, torch.nn.Module: Convolution and pooling layers.
+        fc, torch.nn.Module: 200.
+    """
+    def __init__(self, pretrained=False):
+        """Declare all needed layers."""
+        torch.nn.Module.__init__(self)
+        # Convolution and pooling layers of VGG-16.
+        self.features = torchvision.models.vgg16(pretrained=pretrained).features
+        self.features = torch.nn.Sequential(*list(self.features.children())
+                                            [:-1])  # Remove pool5.
+        # Linear classifier.
+        self.fc = torch.nn.Linear(512**2, 200)
+
+        # Freeze all previous layers.
+        if pretrained:
+            for param in self.features.parameters():
+                param.requires_grad = False
+            def init_weights(layer):
+                if type(layer) == torch.nn.Conv2d or type(layer) == torch.nn.Linear:
+                    torch.nn.init.kaiming_normal_(layer.weight.data)
+                    if layer.bias is not None:
+                        torch.nn.init.constant_(layer.bias.data, val=0)
+            self.fc.apply(init_weights)
+            self.trainable_params = [
+                {'params': self.fc.parameters()}
+            ]
+
+    def forward(self, X):
+        """Forward pass of the network.
+
+        Args:
+            X, torch.autograd.Variable of shape N*3*448*448.
+
+        Returns:
+            Score, torch.autograd.Variable of shape N*200.
+        """
+        N = X.size()[0]
+        assert X.size() == (N, 3, 448, 448)
+        X = self.features(X)
+        assert X.size() == (N, 512, 28, 28)
+        X = X.view(N, 512, 28**2)
+        X = torch.bmm(X, torch.transpose(X, 1, 2)) / (28**2)  # Bilinear
+        assert X.size() == (N, 512, 512)
+        X = X.view(N, 512**2)
+        X = torch.sqrt(X + 1e-5)
+        X = torch.nn.functional.normalize(X)
+        X = self.fc(X)
+        assert X.size() == (N, 200)
+        return X
